fb90fc36a7f290a26907bceb15049863ed0c4dcc
junais <junu743@gmail.com>
2016-07-27 15:36:44 +0530

    Fixed errors

--- src/lib/Authentication/Message.cpp
+++ src/lib/Authentication/Message.cpp
@@ -15,7 +15,7 @@ namespace Santiago{ namespace Authentication
     ConnectionMessage::ConnectionMessage(const std::string& inputString_)
     {
         int size = inputString_.size();
-        std::string typeString = inputString_.substr(0, 4); //substr(4, 4) if no.of bytes is there in inputString_
+        std::string typeString = inputString_.substr(4, 4);
         std::map<int, MessageType> type;
         type = {{65, MessageType::NEW_USER},
                 {66, MessageType::LOGIN_USER},
@@ -23,16 +23,14 @@ namespace Santiago{ namespace Authentication
                 {68, MessageType::CHANGE_PASS_USER},
                 {69, MessageType::CHANGE_PASS_AND_LOGOUT_USER},
                 {70, MessageType::VERIFY_USER}};
-        int typeNum = *(reinterpret_cast<const unsigned*>(typeString.c_str()));
-        _type = type.find(typeNum)->second;
-        //if no.of parameters is required
-        /*std::string parametersNumString = inputString_.substr(4, 4); //substr(8, 4) if no.of bytes is there in inputString_
-          int parameterNum = *(reinterpret_cast<const unsigned*>(parametersNumString.c_str()))*/
-        int startPos = 8; //startPos = 12 if no.of bytes is there in inputString_
+        _type = type.find(*(int*)typeString.c_str())->second;
+        // std::string parametersNumString = inputString_.substr(8, 4);
+        //  int parametersNum = *(int*)parametersNumString.c_str();
+        int startPos = 12;
         while(startPos < size)
         {
-            std::string parameterSizeString = inputString_.substr(startPos, 4);            
-            int parameterSize = *(reinterpret_cast<const unsigned*>(parameterSizeString.c_str()));
+            std::string parameterSizeString = inputString_.substr(startPos, 4);
+            int parameterSize = *(int*)parameterSizeString.c_str();
             startPos += 4;
             std::string parameter = inputString_.substr(startPos, parameterSize);
             _parameters.push_back(parameter);
@@ -52,13 +50,13 @@ namespace Santiago{ namespace Authentication
                     {MessageType::CHANGE_PASS_AND_LOGOUT_USER, 69},
                     {MessageType::VERIFY_USER, 70}};
         int type = enumType.find(_type)->second;
-        messageString.write(reinterpret_cast<char*>(&type), sizeof(int));
-        int parameterSize = _parameters.size();
-        messageString.write(reinterpret_cast<char*>(&parameterSize), sizeof(int));
+        messageString.write((const char*)&type, sizeof(type));
+        int parasize = _parameters.size();
+        messageString.write((const char*)&(parasize), sizeof(parasize));
         for(auto it = _parameters.begin(); it != _parameters.end(); ++it)
         {
             int size = (*it).size();
-            messageString.write(reinterpret_cast<char*>(&size), sizeof(int));
+            messageString.write((const char*)&(size), sizeof(size));
             messageString << *it; 
         }
         return messageString.str();
