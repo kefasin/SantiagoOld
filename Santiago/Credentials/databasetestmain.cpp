676329f87400fd3e72dec221f7101c215ec025b7
vijayaraj <vijayaraj@kefassoftwares.com>
2016-07-28 13:29:13 +0530

    class Records removed

--- Santiago/Credentials/databasetestmain.cpp
+++ Santiago/Credentials/databasetestmain.cpp
@@ -7,25 +7,25 @@ using namespace Santiago::Database;
 int main()
 {
     Santiago::Database::MariaDBConnections connection;
-    Records::UserPermission x = Records::UserPermission::READ_WRITE;
-    connection.addPermissionRecord(34, "safek", x);
-    connection.updateUserPassword("junais", "saitan","junki");
-    connection.addSessionRecord("dfd", "kefas",
-    {date{2016, 5, 12}, time_duration{12, 0, 0}});
-    connection.updateSessionRecord("dfd", {date{2014, 6, 12}, time_duration{13, 0, 3}});
+    Records::UserPermission x = UserPermission::READ_WRITE;
     connection.addUserProfileRecord("junais", "pakistan");
-    std::vector<Records::UserProfile> records1 = connection.getUserProfileRecord();
+    connection.addSessionRecord("junais", "kefas",
+    {date{2016, 5, 12}, time_duration{12, 0, 0}});
+    connection.updateSessionRecord("junais", {date{2014, 6, 12}, time_duration{13, 0, 3}});
+    connection.addPermissionRecord(34, "junais", x);
+    connection.updateUserPassword("junais", "pakistan","india");
+    std::vector<UserProfile> records1 = connection.getUserProfileRecord();
     for(auto it = records1.begin(); it != records1.end(); ++it)
     {
         std::cout << "\n" << (*it)._id << "\t" << (*it)._userName << "\t" << (*it)._password << "\n";
     }
-    std::vector<Records::Session> records2 = connection.getSessionRecord();
+    std::vector<Session> records2 = connection.getSessionRecord();
     for(auto it = records2.begin(); it != records2.end(); ++it)
     {
         std::cout << "\n" << (*it)._id << "\t" << (*it)._cookieId <<
             "\t" << (*it)._userName << "\t" << (*it)._loginTime << "\t" << (*it)._logoutTime << "\n";
             }
-    std::vector<Records::Permission> records3 = connection.getPermissionRecord();
+    std::vector<Permission> records3 = connection.getPermissionRecord();
     for(auto it = records3.begin(); it != records3.end(); ++it)
     {
         std::cout << "\n" << (*it)._id << "\t" << (*it)._resId <<
