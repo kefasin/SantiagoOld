5704497d5da230ae81a0dcea5244716420f97288
junais <junu743@gmail.com>
2016-07-29 16:52:09 +0530

    TCPConnection test

--- src/test/Authentication/TCPConnectionTest.cpp
+++ src/test/Authentication/TCPConnectionTest.cpp
@@ -1,19 +1,56 @@
 #include "TCPConnection.h"
+#include <memory>
+#include <iostream>
 using boost::asio::ip::tcp;
+using namespace Santiago::Authentication;
 
-namespace Santiago{namespace Authentication
+void handleDisconnect();
+void handleClientMessage();
+std::function<void()> onDisconnectCallbackFn =
+    std::bind(&handleDisconnect);
+std::function<void(const ConnectionMessage)> onMessageCallbackFn =
+    std::bind(&handleClientMessage);
+void start();
+void handleAccept(const TCPConnection::MySocketPtr socketPtr_,
+                  const boost::system::error_code& error_);
+std::shared_ptr<tcp::acceptor> _acceptorPtr;
+
+int main()
 {
-   
-    std::function<void()> onDisconnectCallbackFn;
-    std::function<void(const ConnectionMessage)> onMessageCallbackFn;
-    int main()
+
+    boost::asio::io_service io_service;
+    _acceptorPtr.reset(new tcp::acceptor(io_service, tcp::endpoint(tcp::v4(), 3600)));
+    start();                                              
+}
+
+
+void start()
+{
+    TCPConnection::MySocketPtr socketPtr(new TCPConnection::MySocket(_acceptorPtr->get_io_service()));
+    _acceptorPtr->async_accept(*socketPtr,
+                           boost::bind(&handleAccept, socketPtr,
+                                       boost::asio::placeholders::error));
+}
+
+void handleAccept(const TCPConnection::MySocketPtr socketPtr_,
+                          const boost::system::error_code& error_)
+{
+    if (!error_)
     {
-        boost::asio::io_service io_service;
-        tcp::acceptor _acceptor(io_service, tcp::endpoint(tcp::v4(), 3000));
-        TCPConnection::MySocketPtr socketPtr(new TCPConnection::MySocket(_acceptor.get_io_service()));
-        TCPConnection::Ptr newConnection(new TCPConnection(socketPtr
-                                                           ,onDisconnectCallbackFn
-                                                           ,onMessageCallbackFn));
-    }
+
+        TCPConnection::Ptr newConnection(new TCPConnection(socketPtr_,onDisconnectCallbackFn,
+                                                         onMessageCallbackFn));
+        start();
     }
 }
+            
+void handleDisconnect()
+{
+    std::cout<<"handleDisconnect"<<std::endl;
+}
+
+void handleClientMessage()
+{
+    std::cout<<"handleClientMessage"<<std::endl;
+}
+
