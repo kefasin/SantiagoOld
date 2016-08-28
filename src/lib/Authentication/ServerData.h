67860b22201ae5e9f8aeab0200da046d8d94922a
wilson <wilson@kefassoftwares.com>
2016-08-28 23:07:43 +0530

    New Requests enums.

--- src/lib/Authentication/ServerData.h
+++ src/lib/Authentication/ServerData.h
@@ -5,11 +5,18 @@
 
 namespace Santiago{ namespace Authentication
 {
+    struct ResourceData
+    {
+        std::vector<std::pair<unsigned,std::string> >  _connectionIdCookiePairList;
+    };
+
     struct ServerData
     {
-        std::map<std::string,CookieData>           _cookieCookieDataMap;
-        std::map<std::string,UserIdData>           _userIdUserIdDataMap;
-        std::map<RequestId,RequestHandlerBasePtr>  _activeRequestHandlersList;
+        std::map<std::string,CookieData>                 _cookieCookieDataMap;
+        std::map<std::string,UserIdData>                 _userIdUserIdDataMap;
+        std::map<RequestId,RequestHandlerBasePtr>        _activeRequestHandlersList;
+        std::map<std::string,ResourceData>               _resourceIdResourceDataMap;
+        std::map<std::string,std::vector<std::string> >  _cookieIdResourceIdMap;
     };
 }}
 
