b06ac0329eb41a9ec8194eae25e86237ee3cf9d7
vijayaraj <vijayaraj@kefassoftwares.com>
2016-08-29 13:15:54 +0530

    RequestHandler

---
+++ src/lib/Authentication/AddResourceRequestHandler.cpp
@@ -0,0 +1,41 @@
+#include " AddResourceRequestHandler.h"
+
+namespace Santiago{ namespace Authentication
+{
+
+    AddResourceRequestHandler:: AddResourceRequestHandler(ConnectionServer& connectionServer_
+                                                          ,const OnCompletedCallbackFn& onCompletedCallbackFn_
+                                                          ,const ServerMessage& initiatingMessage_)
+        :RequestHandlerBase(connectionServer_,onCompletedCallbackFn_,initiatingMessage_)
+    {}
+    
+    virtual void  AddResourceRequestHandler::start()
+    {
+        if(_databaseConnector.addPermissionrecord(_initiatingMessage._connectionMessage._parameters[0],
+                                                  _initiatingMessage._connectionMessage._parameters[1],
+                                                  _initiatingMessage._connectionMessage._parameters[2]))
+        {
+            ServerMessage serverMessage(_initiatingMessage._connectionId
+                                        ,_initiatingMessage._requestId
+                                        ,_initiatingMessage._type.CONNECTION_MESSAGE_REPLY
+                                        ,_initiatingMessage->_connectionMessage(SUCCEEDED,std::vector<std::string>));
+            
+            _connectionServer.sendMessage(serverMessage);
+            _onCompletedCallbackFn(_initiatingMessage._requestId);
+        }
+        
+        else
+        {
+            ServerMessage serverMessage(_initiatingMessage._connectionId
+                                        ,_initiatingMessage._requestId
+                                        ,_initiatingMessage._type.CONNECTION_MESSAGE_REPLY
+                                        ,_initiatingMessage->_connectionMessage(FAILED,std::vector<std::string>));
+        }   
+    }
+    
+    virtual void  AddResourceRequestHandler::handleReplyMessage(const ServerMessage& serverMessage)
+    {
+        BOOST_ASSERT(false);
+    }
+
+}}
